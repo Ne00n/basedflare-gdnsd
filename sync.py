712f9b2c3db30f207751867e6fe449b8db75d700
Neoon <neo@tiefkuehler-madness.me>
2023-10-09 21:23:17 +0200

    generate zone files

--- sync.py
+++ sync.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import subprocess, tldextract
+import subprocess, tldextract, os
 
 client = MongoClient() 
 client = MongoClient("mongodb://localhost:27017/") 
@@ -15,11 +15,27 @@ for user in cursor:
         if not ext.registered_domain in domains: domains[ext.registered_domain] = []
         if ext.subdomain: domains[ext.registered_domain].append(domain)
 
+def gdnsdZone(domains):
+        template = f'''$TTL 86400
+@     SOA ns1.dns.com admin. (
+      1      ; serial
+      7200   ; refresh
+      30M    ; retry
+      3D     ; expire
+      900    ; ncache
+)
+
+@   NS	ns1.dns.com.
+@   NS	ns2.dns.com.
+'''
+        for domain in domains: template += f"{domain}     30    DYNA    geoip!geo_www\n"
+        return template
+
 #update zones
 gdnsdZonesDir = "/etc/gdnsd/zones"
 files,current,reload = os.listdir(gdnsdZonesDir),[],False
 for domain,subdomains in domains.items():
-    zone = ""
+    zone = gdnsdZone(subdomains)
     with open(f"{gdnsdZonesDir}/{domain}", 'w') as out: out.write(zone)
     current.append(domain)
     reload = True
