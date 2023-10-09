d7825eabe074a4e85458703c0ccfc9a1cc439d4b
Neoon <neo@tiefkuehler-madness.me>
2023-10-09 21:56:06 +0200

    run in a loop

--- sync.py
+++ sync.py
@@ -1,52 +1,61 @@
 from pymongo import MongoClient
-import subprocess, tldextract, json, os
+import subprocess, tldextract, hashlib, json, time, os
 
 with open('config.json', 'r') as f: config = json.load(f)
-
-client = MongoClient() 
 client = MongoClient(config['mongodb']) 
 
-db = client['test']
-collection = db['accounts']
-cursor = collection.find({})
+cache = {}
+while True:
+    db = client['test']
+    collection = db['accounts']
+    cursor = collection.find({})
 
-domains = {}
-for user in cursor:
-    for domain in user['domains']:
-        ext = tldextract.extract(domain)
-        if not ext.registered_domain in domains: domains[ext.registered_domain] = []
-        if ext.subdomain: domains[ext.registered_domain].append(domain)
+    domains = {}
+    for user in cursor:
+        for domain in user['domains']:
+            ext = tldextract.extract(domain)
+            if not ext.registered_domain in domains: domains[ext.registered_domain] = []
+            if ext.subdomain: domains[ext.registered_domain].append(domain)
 
-def gdnsdZone(domains):
-        nameservers = config['nameservers'].split(",") 
-        template = f'''$TTL 86400
+    def gdnsdZone(domains):
+            nameservers = config['nameservers'].split(",") 
+            template = f'''$TTL 86400
 @     SOA {nameservers[0]} admin. (
-      1      ; serial
-      7200   ; refresh
-      30M    ; retry
-      3D     ; expire
-      900    ; ncache
+    1      ; serial
+    7200   ; refresh
+    30M    ; retry
+    3D     ; expire
+    900    ; ncache
 )
 
 @   NS	{nameservers[0]}.
 @   NS	{nameservers[1]}.
 '''
-        for domain in domains: template += f"{domain}     30    DYNA    geoip!geo_www\n"
-        return template
+            for domain in domains: template += f"{domain}     30    DYNA    geoip!geo_www\n"
+            return template
 
-#update zones
-gdnsdZonesDir = "/etc/gdnsd/zones"
-files,current,reload = os.listdir(gdnsdZonesDir),[],False
-for domain,subdomains in domains.items():
-    zone = gdnsdZone(subdomains)
-    with open(f"{gdnsdZonesDir}/{domain}", 'w') as out: out.write(zone)
-    current.append(domain)
-    reload = True
+    #update zones
+    gdnsdZonesDir = "/etc/gdnsd/zones"
+    current,reload = [],False
 
-#domains removed from database
-for file in files:
-    if file not in current:
-        os.remove(f"{gdnsdZonesDir}/{file}")
-        reload = True
+    for domain,subdomains in domains.items():
+        zone = gdnsdZone(subdomains)
+        current.append(domain)
+        currentZoneHash = hashlib.sha256(zone.encode('utf-8')).hexdigest()
+        if not domain in cache: 
+            cache[domain] = currentZoneHash
+            reload = True
+        if currentZoneHash != cache[domain]: reload = True
+        if reload: 
+            with open(f"{gdnsdZonesDir}/{domain}", 'w') as out: out.write(zone)
 
-if reload: subprocess.run(["/usr/bin/sudo", "/usr/bin/gdnsdctl", "reload-zones"])
+    files = os.listdir(gdnsdZonesDir)
+    #domains removed from database
+    for file in files:
+        if file not in current:
+            os.remove(f"{gdnsdZonesDir}/{file}")
+            reload = True
+
+    if reload: subprocess.run(["/usr/bin/sudo", "/usr/bin/gdnsdctl", "reload-zones"])
+    reload = False
+    time.sleep(1)
